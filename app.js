3c0bc433c9ce6041c6ea8e7f63ff612cf62304fa
Jaime Jacobo <jaimejacobordv@gmail.com>
2018-11-28 15:40:13 -0500

    x

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const User         = require('./models/User');
 
 
 mongoose
-  .connect('mongodb://localhost/myly-quotes', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
